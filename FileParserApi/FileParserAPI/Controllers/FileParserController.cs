465c32cd2896652302ca4f30f56af85532849d98
friedz05 <james.friedenberger@gmail.com>
2020-01-26 10:28:31 -0600

    Simplify get responses and update post to include whether the raw data was parsed.  Updated commenting and some code cleanup

--- FileParserApi/FileParserAPI/Controllers/FileParserController.cs
+++ FileParserApi/FileParserAPI/Controllers/FileParserController.cs
@@ -1,10 +1,8 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace FileParserAPI.Controllers
 {
@@ -14,60 +12,61 @@ namespace FileParserAPI.Controllers
     {
         private readonly ILogger<FileParserController> _logger;
         
-
         public FileParserController(ILogger<FileParserController> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Simple method that returns name of api
+        /// </summary>
+        /// <returns>name of api</returns>
         [HttpGet]
         public string Get()
         {
             return "FileParser";
         }
 
-
+        /// <summary>
+        /// Get method returns the record data sorted by gender
+        /// </summary>
+        /// <returns>Json response with data sorted</returns>
         [HttpGet("/records/gender")]
-        public string[] GetByGender()
+        public ActionResult GetByGender()
         {
-            
-            var result = new List<string>();
-            foreach (var record in RecordContainer.ApiRecords.OrderBy(x => x.Gender))
-            {
-                result.Add(Newtonsoft.Json.JsonConvert.SerializeObject(record, Formatting.None));
-            }
-            return result.ToArray();
-
+            return Ok(RecordContainer.ApiRecords.OrderBy(x => x.Gender));
         }
-
+        /// <summary>
+        /// Get method returns the record data sorted by Date of Birth
+        /// </summary>
+        /// <returns>Json response with data sorted</returns>
         [HttpGet("/records/birthdate")]
-        public string[] GetByBirthDate()
+        public ActionResult GetByBirthDate()
         {
-            var result = new List<string>();
-            foreach (var record in RecordContainer.ApiRecords.OrderBy(x => x.DOB))
-            {
-                result.Add(Newtonsoft.Json.JsonConvert.SerializeObject(record, Formatting.None));
-            }
-            return result.ToArray();
+            return Ok(RecordContainer.ApiRecords.OrderBy(x => x.DOB));
         }
-
+        /// <summary>
+        /// Get method returns the record data sorted by Last Name
+        /// </summary>
+        /// <returns>Json response with data sorted</returns>
         [HttpGet("/records/name")]
-        public string[] GetByName()
+        public ActionResult GetByName()
         {
-            var result = new List<string>();
-            foreach (var record in RecordContainer.ApiRecords.OrderBy(x => x.LastName))
-            {
-                result.Add(Newtonsoft.Json.JsonConvert.SerializeObject(record, Formatting.None));
-            }
-            return result.ToArray();
+            return Ok(RecordContainer.ApiRecords.OrderBy(x => x.LastName));
         }
 
+        /// <summary>
+        /// Post method that grabs the data from the body of the request, attempts to parse it
+        /// If parsed successful, data is added to singleton
+        /// </summary>
+        /// <returns>String "Content Posted" if parsed successfully, "Content Not Parsed" otherwise</returns>
         [HttpPost("/records")]
         public async Task<string> Post()
         {
             var content = new string[] { await new StreamReader(Request.Body).ReadToEndAsync() };
+            var initCount = RecordContainer.ApiRecords.Count;
             RecordContainer.ApiRecords.AddRange(FileParser.FileParser.ParseLines(content));
-            return "Success";
+            return initCount < RecordContainer.ApiRecords.Count ? "Content Posted" : "Content Not Parsed";
         }
     }
 }
